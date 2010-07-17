21fe7bc80236b3abcd2609ead12b7e22e31b02f7
jomora <jomora@4ed120a7-bdca-d447-8275-f09b7f835e33>
2010-07-17 09:24:24 +0000

    errorlog.php のテキスト版とRSS版を統合

--- ApacheUtility/error.log/errorlog.php
+++ ApacheUtility/error.log/errorlog.php
@@ -2,29 +2,31 @@
 //
 // Apache error.log Viewer
 //
-// 2005.03.21 Jomora created
-// 2005.08.05 Jomora if (count($logs) > $maxLineSize) èåèí«â¡
+// @author Jomora ( kazuhiko@jomura.net http://jomura.net/ )
+// @version 2008.03.15 Jomura Add <link> tag
+//          2005.08.05 Jomora if (count($logs) > $maxLineSize) Êù°‰ª∂ËøΩÂä†
+//          2005.03.21 Jomora created
 
-@header("Content-Type: text/plain;");
 
-// ÉçÉOÉtÉ@ÉCÉãÇÃéwíË
-$logFile = "C:/Apache/logs/error.log";
+// „É≠„Ç∞„Éï„Ç°„Ç§„É´„ÅÆÊåáÂÆö
+$logFile = "C:/Server/Apache/logs/error.log";
 
-// ï\é¶Çè»ó™Ç∑ÇÈÉçÉO
+// Ë°®Á§∫„ÇíÁúÅÁï•„Åô„Çã„É≠„Ç∞
 $omittedLogs = array(
+  "(20024)The given path is misformatted or contained invalid characters: Cannot map GET /" => 0,
   "client denied by server configuration:" => 0,
   "File does not exist:" => 0,
   "not found or unable to stat" => 0
 );
 
-// ç≈ëÂçsêî
+// ÊúÄÂ§ßË°åÊï∞
 $maxLineSize = 50;
-// 1çsÇÃç≈ëÂï∂éöêî
+// 1Ë°å„ÅÆÊúÄÂ§ßÊñáÂ≠óÊï∞
 $maxLength = 4096;
 
 $logs = array();
 
-// ï\é¶Ç∑ÇÈÉçÉOÉtÉ@ÉCÉãÇÃéwíË
+// Ë°®Á§∫„Åô„Çã„É≠„Ç∞„Éï„Ç°„Ç§„É´„ÅÆÊåáÂÆö
 $handle = fopen ($logFile, "rt");
 while (!feof ($handle)) {
     $buffer = fgets($handle, $maxLength);
@@ -43,23 +45,85 @@ while (!feof ($handle)) {
 	}
 
     if ($canView) {
-        $logs[] = $buffer;
+        $logs[] = mb_convert_encoding($buffer, "UTF-8", "SJIS-win");
     }
 }
 fclose ($handle);
 
-// ï\é¶Çè»ó™ÇµÇΩÉçÉOÇÃÉJÉEÉìÉg
-foreach($omittedLogs as  $key => $value) {
-	print($key . ' ' . $value . "\n");
-}
-print("\n");
-// ç≈êVÇÃÉçÉOÇ©ÇÁç≈ëÂï\é¶çsêîï™ÇæÇØï\é¶Ç∑ÇÈ
+// ÊúÄÊñ∞„ÅÆ„É≠„Ç∞„Åã„ÇâÊúÄÂ§ßË°®Á§∫Ë°åÊï∞ÂàÜ„Å†„ÅëË°®Á§∫„Åô„Çã
 if (count($logs) > $maxLineSize) {
 	$logs = array_slice($logs, count($logs) - $maxLineSize);
 }
-// êVÇµÇ¢Ç‡ÇÃÇ©ÇÁèáÇ…ï\é¶Ç∑ÇÈÅB
+// Êñ∞„Åó„ÅÑ„ÇÇ„ÅÆ„Åã„ÇâË°®Á§∫
 $logs = array_reverse($logs);
-foreach ($logs as $log) {
-    print htmlspecialchars($log);
+
+
+// ‰ª•‰∏ã„ÄÅÂøúÁ≠îÈÉ®
+
+if (isset($_GET["format"]) && "rss" === $_GET["format"]) {
+	// RSS
+	header('Content-type: text/xml; charset=UTF-8');
+?>
+<?xml version="1.0"?>
+<rss version="2.0">
+   <channel>
+      <title>Apache error.log</title>
+      <link>http://jomura.net/wiki/?Apache%2Ferror.log%E3%82%92%E3%83%AA%E3%83%A2%E3%83%BC%E3%83%88%E3%81%8B%E3%82%89%E9%96%B2%E8%A6%A7</link>
+      <description>
+<?php 
+	// Ë°®Á§∫„ÇíÁúÅÁï•„Åó„Åü„É≠„Ç∞„ÅÆ„Ç´„Ç¶„É≥„Éà
+	foreach($omittedLogs as  $key => $value) {
+		print(sprintf('% 5d', $value) . ' ' . $key . "<br />\n");
+	}
+?>
+      </description>
+<?php
+	foreach ($logs as $log) {
+		$title = NULL;
+		$pubDate = NULL;
+		$guid = NULL;
+		$canDate = "[" == substr($log,0,1);
+		if ($canDate) {
+			$title = htmlspecialchars(substr($log,27));
+			$pubDate = date(DATE_RSS, strtotime(substr($log,1,24)));
+			$guid = date(DATE_ATOM, strtotime(substr($log,1,24)));
+		} else {
+			$title = htmlspecialchars($log);
+		}
+		//$query = urlencode(trim(ereg_replace("\[[^\[\]]+\]", "", $title)));
+		//$linkUrl = "http://www.google.co.jp/search?q=" . $query;
+		$linkUrl = "http://jomura.net/errorlog.php";
+?>
+      <item>
+         <title><?php echo $title; ?></title>
+         <link><?php echo $linkUrl; ?></link>
+<?php
+	if ($canDate) {
+?>
+         <pubDate><?php echo $pubDate; ?></pubDate>
+         <guid isPermaLink="false"><?php echo $guid; ?></guid>
+<?php
+	}
+?>
+      </item>
+<?php
+}
+?>
+   </channel>
+</rss>
+<?php
+} else {
+	// TEXT
+	@header("Content-Type: text/plain; charset=UTF-8");
+
+	// Ë°®Á§∫„ÇíÁúÅÁï•„Åó„Åü„É≠„Ç∞„ÅÆ„Ç´„Ç¶„É≥„Éà
+	foreach($omittedLogs as  $key => $value) {
+		print(sprintf('% 5d', $value) . ' ' . $key . "\n");
+	}
+	print("\n");
+
+	foreach ($logs as $log) {
+	    print htmlspecialchars($log);
+	}
 }
 ?>
