5a9ced69a72671f7fe838d745a3a454387535500
Jomora <42256916+joumura@users.noreply.github.com>
2024-08-25 08:27:41 +0900

    Add files via upload

--- iTunes/iTunesの全playlistをexportする.js
+++ iTunes/iTunesの全playlistをexportする.js
@@ -5,15 +5,26 @@
  * @version 2024.05.03 init
  */
 
+WScript.Echo("start : " + new Date());
+
 // config
-var libRootPath = "E:\\iTunes\\iTunes Media\\Music\\";
+var libRootPath = "E:/iTunes/iTunes Media/Music/";
+var playlistPath = "E:/iTunes/iTunes Media/playlists/";
 
 // const
 var adostrm = WScript.CreateObject("ADODB.Stream");
 adostrm.Charset = "UTF-8";   // with BOM
+var fs = WScript.CreateObject("Scripting.FileSystemObject");
 var iTunesApp = WScript.CreateObject("iTunes.Application");
 
 // main
+
+for (var e = new Enumerator(fs.GetFolder(playlistPath).Files); !e.atEnd(); e.moveNext()){
+    if (e.item().Name.match("\.m3u$")) {
+        fs.DeleteFile(e.item());
+    }
+}
+
 for (var i = 1; i <= iTunesApp.LibrarySource.Playlists.Count; i++) {
     var plist = iTunesApp.LibrarySource.Playlists.Item(i);
     WScript.Echo(plist.Name + " : " + plist.Tracks.Count);
@@ -33,6 +44,10 @@ for (var i = 1; i <= iTunesApp.LibrarySource.Playlists.Count; i++) {
         location = location.replace(libRootPath, "");
         adostrm.WriteText(location, 1);
     }
-    adostrm.SaveToFile(plist.Name + ".m3u", 2);
+    adostrm.SaveToFile(playlistPath + plist.Name + ".m3u", 2);
     adostrm.Close();
 }
+
+iTunesApp.Quit();
+
+WScript.Echo("end : " + new Date());
